b8fcf2f79f5e7ff5683f8d8f65d6dd9f81534ab0
Gabriel <gabriel.kpaz@gmail.com>
2025-06-07 07:55:35 -0300

    700 linhas pra um slide

--- T3/calculadoras/haskell_calculator/script_h.sh
+++ T3/calculadoras/haskell_calculator/script_h.sh
@@ -2,22 +2,17 @@
 
 echo "🔧 Instalando dependências da calculadora Haskell..."
 
-# Atualiza os repositórios
 sudo apt update
 
-# Instala GHC (compilador Haskell) e o Cabal
 sudo apt install -y ghc cabal-install make
 
-# Atualiza o índice de pacotes do Cabal
 cabal update
 
 echo "✅ Ambiente Haskell instalado com sucesso!"
 
-# Compila automaticamente usando o Makefile
 echo "⚙️ Compilando o projeto com make..."
 make
 
-# Verifica se o executável foi gerado
 if [ -f "calc" ]; then
     echo "🚀 Compilação concluída com sucesso! Execute com: ./calc"
 else
