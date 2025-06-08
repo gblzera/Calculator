4a9bc146848f63e191581add09da6b4623e35a69
Gabriel <gabriel.kpaz@gmail.com>
2025-06-08 13:13:21 -0300

    glimpse of us

--- T3/calculadoras/prolog_calculator/calculator.pl
+++ T3/calculadoras/prolog_calculator/calculator.pl
@@ -17,14 +17,3 @@ handle_input(Input) :-
     ; true
     ),
     start.
-
-% is em prolog não é apenas uma atribuição simples, ele é um avaliador arithmetico automatico.
-% a escrita de Result is Input, o prolog: Pega a expressão em Input (exemplo 2+3*4), avalia automaticamente seguindo as regras matematicas e calcula o restulado e unifica com Result.
-% Por que não precisa programar +, -, *, /?
-% O Prolog já tem essas operações built-in! O is reconhece automaticamente:
-% + (adição)
-% - (subtração)
-% * (multiplicação)
-% / (divisão)
-% ** (exponenciação)
-% mod (módulo)
